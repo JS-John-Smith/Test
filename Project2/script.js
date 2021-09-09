a314da74b593f7522ef7cdfdecc474e56c388bc4
John <jsfakemail@mail.ru>
2021-09-09 18:15:52 +0300

    Test message 1

---
+++ Project2/script.js
@@ -0,0 +1,14 @@
+'use strict'
+// console.log(4 / 0);
+// console.log('string' * 9);
+// let string;
+// console.log(string);
+
+var obj = {
+    name: 'John',
+    age:  25
+}
+console.log(obj.age);
+console.log(obj['name']);
+var arr = ['John', 30];
+console.log(arr[1]);
